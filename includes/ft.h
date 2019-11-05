6a3e0590350f8a7400433298b43c0b833abb677c
Lila Fallet <lilafallet@gmail.com>
2019-11-05 10:55:31 +0100

    add ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -23,5 +23,6 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
