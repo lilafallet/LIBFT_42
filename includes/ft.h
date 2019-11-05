3e9ef3aaeedae1a0f3dfd3dc44e1b4596faaadb7
Lila Fallet <lilafallet@gmail.com>
2019-11-05 12:47:26 +0100

    add ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -25,5 +25,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
