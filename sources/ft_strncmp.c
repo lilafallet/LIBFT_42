72526044774e340a2b2d405edd9c7b63c4e4410b
Lila Fallet <lilafallet@gmail.com>
2019-11-05 10:56:03 +0100

    add ft_strncmp

--- sources/ft_strncmp.c
+++ sources/ft_strncmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft.c"
+#include "ft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
