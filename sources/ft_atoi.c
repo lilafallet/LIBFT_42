7b3f35134b4e43b23706f0dd0371726fbe7e628c
Lila Fallet <lilafallet@gmail.com>
2019-11-06 09:56:42 +0100

    add ft_atoi.c

---
+++ sources/ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+	int	min;
+
+	i = 0;
+	min = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			min = -min;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * min);
+}
