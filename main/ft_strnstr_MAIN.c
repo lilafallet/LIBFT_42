9c601ef9c8617f7300b01240d597cf87686085de
Lila Fallet <lilafallet@gmail.com>
2019-11-06 09:24:39 +0100

    add ft_strnstr_MAIN

---
+++ main/ft_strnstr_MAIN.c
@@ -0,0 +1,12 @@
+#include "ft.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	char	*new_str;
+	
+	(void)ac;
+	new_str = ft_strnstr(av[1], av[2], atoi(av[3]));
+	puts(new_str);
+	return (EXIT_SUCCESS);
+}
