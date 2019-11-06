e5949cb836b113a30a56144b6dbce8c624027bcc
Lila Fallet <lilafallet@gmail.com>
2019-11-06 19:40:04 +0100

    [DEV] ft.h : add ft_memccpy.c

--- includes/ft.h
+++ includes/ft.h
@@ -36,6 +36,6 @@ void	ft_bzero(void *s, size_t n);
 int		ft_isascii(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 
 #endif
