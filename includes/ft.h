58492561fac6e17f6761a023e2fd6cedc36f2f7a
Lila Fallet <lilafallet@gmail.com>
2019-11-06 15:12:49 +0100

    add ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -33,5 +33,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+int		ft_isascii(int c);
 
 #endif
