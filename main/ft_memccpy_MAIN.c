bd24f3fc14100e31a80569555a680a7ecca5d699
Lila Fallet <lilafallet@gmail.com>
2019-11-06 19:41:22 +0100

    [LAST] > ft_memccpy_MAIN.c : last modification

---
+++ main/ft_memccpy_MAIN.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "ft.h"
+
+int	main(void)
+{
+	char	str1[100] = "bonjour";
+	
+	printf("%s\n", (char *)memccpy(str1, str1, 'a', 4));
+	printf("%s\n", (char *)ft_memccpy(str1, str1, 'a', 4)); 
+	return (EXIT_SUCCESS);
+}
