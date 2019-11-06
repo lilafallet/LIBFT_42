d18eeeb8bfb19396df6ceba0c43efccbff10d8a3
Lila Fallet <lilafallet@gmail.com>
2019-11-06 14:32:28 +0100

    add ft_strncmp.c

--- sources/ft_strncmp.c
+++ sources/ft_strncmp.c
@@ -19,10 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] > s2[i])
-			return (1);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
