6201d9664401e745e286248ff408d01860a38cab
Lila Fallet <lilafallet@gmail.com>
2019-11-06 09:25:03 +0100

    add ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -26,5 +26,6 @@ char	*ft_strchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
