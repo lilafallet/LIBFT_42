dd11aaeba548125d2256c6fec3ed71e46e41722e
Lila Fallet <lilafallet@gmail.com>
2019-11-07 23:12:18 +0100

    [LAST] > ft.h : add strtrim

--- includes/ft.h
+++ includes/ft.h
@@ -45,12 +45,15 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+
 /*
 **** PART 2
 */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+
 /*
 **** Personal Part
 */
