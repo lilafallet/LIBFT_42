ff9d3d0333fe46ad7958e37214e95a84919765a3
Lila Fallet <lilafallet@gmail.com>
2019-11-07 14:25:42 +0100

    [LAST] : ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -17,6 +17,10 @@
 # define TRUE	1
 # define FALSE	0
 
+/*
+**** PART 1
+*/
+
 size_t	ft_strlen(const char *s);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -38,5 +42,16 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+/*
+**** PART 2
+*/
+
+/*
+**** Personal Part
+*/
+
+
 
 #endif
