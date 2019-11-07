1628fdd6fc912dacd450d51ed33a2c74afe025e2
Lila Fallet <lilafallet@gmail.com>
2019-11-07 23:12:45 +0100

    [LAST] > ft_strtrim_MAIN.c : last modification

---
+++ main/PART2/ft_strtrim_MAIN.c
@@ -0,0 +1,14 @@
+#include "ft.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	char	*str;
+
+	(void)ac;
+	str = ft_strtrim(av[1], av[2]);
+	if (str != NULL)
+		printf("%s\n", str);
+	free(str);
+	return (EXIT_SUCCESS);
+}
