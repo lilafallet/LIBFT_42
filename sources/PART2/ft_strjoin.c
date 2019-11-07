5bd5ec5cdda0cb4828868de313f58ca62c0400ff
Lila Fallet <lilafallet@gmail.com>
2019-11-07 21:42:04 +0100

    [LAST] > ft_strjoin.c : last modification

--- sources/PART2/ft_strjoin.c
+++ sources/PART2/ft_strjoin.c
@@ -7,16 +7,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len_s2;
 	size_t	len_str;
 
-	len_s1 = ft_strlen(s1);	
-	len_s2 = ft_strlen(s2);	
-	len_str = len_s1 + len_s2;
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * len_str + 1);
-	if (str == NULL)
-		return (NULL);
-	ft_memcpy(str, s1, len_s1);
-	ft_memcpy(str + len_s1, s2, len_s2);
-	str[len_str] = '\0';
+	str = NULL;
+	if (s1 != NULL && s2 != NULL)
+	{
+		len_s1 = ft_strlen(s1);	
+		len_s2 = ft_strlen(s2);	
+		len_str = len_s1 + len_s2;
+		str = (char *)malloc(sizeof(char) * (len_str + 1));
+		if (str != NULL)
+		{
+			ft_memcpy(str, s1, len_s1);
+			ft_memcpy(str + len_s1, s2, len_s2);
+			str[len_str] = '\0';
+		}
+	}
+	else if (s1 == NULL)
+		str = ft_strdup(s2);
+	else if (s2 == NULL)
+		str = ft_strdup(s1);
 	return (str);
 }
