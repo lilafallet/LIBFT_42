f768af86e6f7dc5f8834bd3ae35505b1773269e2
Lila Fallet <lilafallet@gmail.com>
2019-11-07 21:42:31 +0100

    [LAST] > ft_substr.c : last modification

--- sources/PART2/ft_substr.c
+++ sources/PART2/ft_substr.c
@@ -4,12 +4,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 
-	if (s == NULL)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (len) + 1);
-	if (str == NULL)
-		return (NULL);
-	ft_memcpy(str, s + start, len);
-	str[len] = '\0';
+	str = NULL;
+	if (s != NULL)
+	{
+		str = (char *)malloc(sizeof(char) * (len + 1));
+		if (str != NULL)
+		{
+			ft_memcpy(str, s + start, len);
+			str[len] = '\0';
+		}
+	}
 	return (str);	
 }
