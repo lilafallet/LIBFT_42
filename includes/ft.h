4c261a216745589cb78885bf627db77907551657
Lila Fallet <lilafallet@gmail.com>
2019-11-07 15:22:08 +0100

    [LAST] : ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -43,6 +43,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 /*
 **** PART 2
