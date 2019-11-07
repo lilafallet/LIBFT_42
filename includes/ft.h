6e116c17cf5022308aa8336392326da2f6f1ded3
Lila Fallet <lilafallet@gmail.com>
2019-11-07 13:56:27 +0100

    [LAST] : ft.h

--- includes/ft.h
+++ includes/ft.h
@@ -37,5 +37,6 @@ int		ft_isascii(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
