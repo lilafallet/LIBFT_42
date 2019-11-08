8d35b3bc06841dec3b9130dff10a87fcd81915f3
Lila Fallet <lilafallet@gmail.com>
2019-11-08 20:04:02 +0100

    [LAST] > ft.h : add putchar_fd

--- includes/ft.h
+++ includes/ft.h
@@ -56,7 +56,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s1, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
+void	ft_putchar_fd(char c, int fd);
 /*
 **** Personal Part
 */
