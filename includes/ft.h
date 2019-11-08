c3e01a7f61e2c402049edc6ae437e98ea6672827
Lila Fallet <lilafallet@gmail.com>
2019-11-08 20:42:15 +0100

    [LAST] > ft.h : add putendl_fd

--- includes/ft.h
+++ includes/ft.h
@@ -58,6 +58,7 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 /*
 **** Personal Part
