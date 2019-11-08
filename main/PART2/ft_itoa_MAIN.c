47ab25b5f3d06b0227eee442688269355df3d2b8
Lila Fallet <lilafallet@gmail.com>
2019-11-08 19:30:53 +0100

    [LAST] > ft_itoa_MAIN.c : last modification

---
+++ main/PART2/ft_itoa_MAIN.c
@@ -0,0 +1,13 @@
+#include "ft.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)\
+{
+	char	*str;
+
+	(void)ac;
+	str = ft_itoa(atoi(av[1]));
+	printf("%s\n", str);
+	free(str);
+	return (EXIT_SUCCESS);
+}
