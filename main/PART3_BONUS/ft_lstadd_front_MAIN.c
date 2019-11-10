d06a6f14c644eb5420df9dc13f131361975f3f5f
Lila Fallet <lilafallet@gmail.com>
2019-11-10 14:28:11 +0100

    [FIX] > ft_lstadd_front.c : fix main test

--- main/PART3_BONUS/ft_lstadd_front_MAIN.c
+++ main/PART3_BONUS/ft_lstadd_front_MAIN.c
@@ -2,11 +2,24 @@
 
 int	main(void)
 {
-	char	*str;
 	t_list	*alst;
+	t_list	*lst;
+	t_list	elem1;
+	t_list	elem2;
+	t_list	elem3;
+	t_list	elem4;
+	t_list	elem5;
+	t_list	elem6;
 
-	str = "lol";
+	lst = &elem1;
+	elem1.next = &elem2;
+	elem2.next = &elem3;
+	elem3.next = &elem4;
+	elem4.next = &elem5;
+	elem5.next = &elem6;
+	elem6.next = NULL;
+
 	alst = NULL;
-	ft_lstadd_front(&alst, str);
+	ft_lstadd_front(&alst, lst);
 	return (EXIT_SUCCESS);
 }
