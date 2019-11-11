38d4a2412d0fefe20c0e792e472bab1d3b655759
Lila Fallet <lilafallet@gmail.com>
2019-11-11 16:01:08 +0100

    [DOC] : add header

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfallet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/04 14:04:05 by lfallet           #+#    #+#             */
-/*   Updated: 2019/11/04 16:56:47 by lfallet          ###   ########.fr       */
+/*   Created: 2019/11/11 15:25:05 by lfallet           #+#    #+#             */
+/*   Updated: 2019/11/11 15:29:07 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,3 @@ int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-int		main(int ac, char **av)
-{
-	if (ac == 2)
-	{
-		printf("%d, %d\n", ft_isalnum(atoi(av[1])), isalnum(atoi(av[1])));
-	}
-	return (0);
-}*/
--- main/PART1/ft_isalnum_MAIN.c
+++ main/PART1/ft_isalnum_MAIN.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnun_MAIN.c                                  :+:      :+:    :+:   */
+/*   ft_isalnum_MAIN.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lfallet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:04:19 by lfallet           #+#    #+#             */
-/*   Updated: 2019/11/04 15:01:57 by lfallet          ###   ########.fr       */
+/*   Updated: 2019/11/11 15:29:07 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	(void)ac;
-	printf("%d\n", ft_isalnum(av[1][0]));
-	return (EXIT_SUCCESS);
+	if (ac == 2)
+	{
+		printf("%d, %d\n", ft_isalnum(atoi(av[1])), isalnum(atoi(av[1])));
+	}
+	return (0);
 }
