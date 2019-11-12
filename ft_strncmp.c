4fd188649a6f173ad7114c84c59534df7a5ce2b9
Lila Fallet <lfallet@student.42.fr>
2019-11-12 15:11:13 +0100

    ft_strncmp.c fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 15:41:19 by lfallet           #+#    #+#             */
-/*   Updated: 2019/11/11 15:41:21 by lfallet          ###   ########.fr       */
+/*   Updated: 2019/11/12 15:09:44 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i] < 0 ? -1 : 1);
 		i++;
 	}
 	return (0);
