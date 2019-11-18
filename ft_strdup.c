ce21f81d351a827189b77b2e2e6b53a07976a7ac
Lila Fallet <lilafallet@gmail.com>
2019-11-18 21:13:40 +0100

    FIX

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 15:39:52 by lfallet           #+#    #+#             */
-/*   Updated: 2019/11/17 17:12:53 by lfallet          ###   ########.fr       */
+/*   Updated: 2019/11/18 21:12:37 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ char	*ft_strdup(const char *s)
 {
 	char	*str;
 	size_t	len_str;
-	size_t	i;
 
-	i = 0;
 	len_str = ft_strlen(s);
 	str = (char *)malloc(sizeof(char) * (len_str + 1));
 	if (str != NULL)
