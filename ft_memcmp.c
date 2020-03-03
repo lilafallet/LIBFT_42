7722052c7fbc861b56461f31c0b21a6aa423b529
Lila Fallet <lfallet@student.42.fr>
2020-03-03 14:36:00 +0100

    add ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 15:36:45 by lfallet           #+#    #+#             */
-/*   Updated: 2019/11/11 15:36:49 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/03/03 14:35:35 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (i < n)
 	{
 		if (((const unsigned char *)s1)[i] != ((const unsigned char *)s2)[i])
+		{
 			return (((const unsigned char *)s1)[i]
 						- ((const unsigned char *)s2)[i]);
+		}
 		i++;
 	}
 	return (0);
