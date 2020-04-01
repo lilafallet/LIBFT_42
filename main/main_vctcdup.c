a772b11c16f31667992cd6859c4fe22cf79058df
Lila Fallet Bayé-Pouey <lilafallet@gmail.com>
2020-04-01 14:58:04 +0200

    begin vct->vct_cdup.c

---
+++ main/main_vctcdup.c
@@ -0,0 +1,43 @@
+//pensez a mettre le header
+
+#include "vector.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	t_vector	*vct1;
+	t_vector	*vct2;
+	t_vector	*vct3;
+	t_vector	*vct4;
+	t_vector	*new1;
+	t_vector	*new2;
+	t_vector	*new3;
+	t_vector	*new4;
+	ssize_t		len;
+
+	if (ac == 5)
+	{
+		vct1 = vct_new();
+		vct2 = vct_new();
+		vct3 = vct_new();
+		vct4 = vct_new();
+		vct_addstr(vct1, av[1]);
+		new1 = vct_cdup(vct1, 'c');
+		printf("new  = %s\n", vct_getstr(new1)); /*debug*/
+		vct_addstr(vct2, av[2]);
+		new2 = vct_cdup(vct2, '1');
+		printf("new  = %s\n", vct_getstr(new2)); /*debug*/
+		vct_addstr(vct3, av[3]);
+		new3 = vct_cdup(vct3, ' ');
+		printf("new  = %s\n", vct_getstr(new3)); /*debug*/
+		vct_addstr(vct4, av[4]);
+		new4 = vct_cdup(vct4, 'j');
+		printf("new  = %s\n", vct_getstr(new4)); /*debug*/
+		vct_del(&vct1);
+		vct_del(&vct2);
+		vct_del(&vct3);
+		vct_del(&vct4);
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
---
+++ sources/vector/vct_cdup.c
@@ -0,0 +1,17 @@
+//pensez a mettre le header
+
+#include "vector.h"
+
+t_vector	*vct_cdup(t_vector *vct, char c)
+{
+	t_vector *new_vct;*
+	size_t	i;
+
+	i = 0;
+	vct = vct_new();
+	if (vct == NULL)
+		return (NULL);
+	while (i < 
+	
+	
+}
