0ff22b8be5eef1daf4bafd171fd617cd50c1bef0
Lila Fallet <lilafallet@gmail.com>
2020-04-05 11:44:52 +0200

    add initialisation_vct ft_vct_split_each

--- sources/vector/vct_chrstr.c
+++ sources/vector/vct_chrstr.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/04 19:32:24 by lfallet           #+#    #+#             */
-/*   Updated: 2020/04/04 20:43:17 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/04/05 11:22:26 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,12 @@ int	vct_chrstr(t_vector *vct, size_t index, char *search)
 {
 	size_t	i;
 
-	while (index < vct->len)
+	i = 0;
+	while (search[i] != '\0')
 	{
-		i = 0;
-		while (search[i] != '\0')
-		{
-			if (vct->str[index] == search[i])
-			{
-				printf("vct->str[%zu] = %c\n", index, vct->str[index]); //
-				return (TRUE);
-			}
-			i++;
-		}
-		index++;
+		if (vct->str[index] == search[i])
+			return (TRUE);
+		i++;
 	}
 	return (FALSE);
 }
--- sources/vector/vct_split_each.c
+++ sources/vector/vct_split_each.c
@@ -6,28 +6,44 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/04 18:47:39 by lfallet           #+#    #+#             */
-/*   Updated: 2020/04/04 20:44:23 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/04/05 11:44:22 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 #include <stdio.h>
+static size_t	initialisation_vct(t_vector *ret_vct, t_vector *vct, size_t i)
+{	
+	printf("i = %zu\n", i); //
+	ft_memmove(ret_vct->str, vct->str, i);
+	ft_memmove(vct->str, vct->str + i + 1, vct->len - i + 1);
+	vct->len = vct->len - i;
+	printf("vct->str = %s\n", vct_getstr(vct)); //
+	return (i);
+}
 
 t_vector	*vct_split_each(t_vector *vct, char *str, int flag)
 {
-	t_vector	*ret_vct;	
-	size_t		i;
+	t_vector			*ret_vct;	
+	static	size_t		i = 0;
+	char				*new_str;
+	size_t				tmp_len;
 
-	i = 0;
+	tmp_len = vct->len;
 	ret_vct = vct_new();
 	if (vct == NULL)
 		return (NULL);
 	while (i < vct->len)
 	{
 		if (vct_chrstr(vct, i, str) == TRUE)
-			i++; //METTRE la chaine dans le vecteur
-		else
+		{
+			while (vct_chrstr(vct, i + 1, str) == TRUE)
+				i++; //faire la difference de i pour pas sauter des caracteres	
+			i = initialisation_vct(ret_vct, vct, i);
 			i++;
+			return (ret_vct);
+		}
+		i++;
 	}
 	vct_del(&ret_vct); //
 	return (NULL);
